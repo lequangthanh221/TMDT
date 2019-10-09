ab099cb4570c581b5f038f71ef44954356a50166
quangthanhle <lequangthanh221@gmail.com>
2019-10-09 13:39:46 +0700

    Finish Interface

---
+++ app/src/main/java/com/example/myapplication/Activity/EditProfileActivity.java
@@ -0,0 +1,16 @@
+package com.example.myapplication.Activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.myapplication.R;
+
+public class EditProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_profile);
+    }
+}
